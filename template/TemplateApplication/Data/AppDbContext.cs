1b8bff7a958c214a3b56902f0bc20d56c0ccd4d7
Marcos Baesse <marcosbaesse@gmail.com>
2021-07-23 21:31:00 -0300

    add: sqlite

---
+++ template/TemplateApplication/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace TemplateApplication.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=MyDatabase.db");
+        }
+    }
+}
--- template/TemplateApplication/Startup.cs
+++ template/TemplateApplication/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TemplateApplication.Data;
 
 namespace TemplateApplication
 {
@@ -24,6 +25,8 @@ namespace TemplateApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddEntityFrameworkSqlite().AddDbContext<AppDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
